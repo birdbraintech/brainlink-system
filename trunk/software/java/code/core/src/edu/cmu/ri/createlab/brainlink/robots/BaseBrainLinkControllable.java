a7fe205bcb23bbb5b687fadcb01797cf2b8d9566
garthabrindoid@gmail.com <garthabrindoid@gmail.com@85f2b622-2cfc-f667-9a34-de0f26db76e9>
2011-01-10 16:45:51 +0000

    Improved connection time by changing the logic in BrainLinkConnectivityManager so that, if the user has specified one or more serial ports, then the code just tries connecting to them, rather than calling SerialPortEnumerator.getAvailableSerialPorts() which will first filter the set of ports down to only those which are available.  The SerialPortEnumerator.getAvailableSerialPorts() method is slow since it has to connect to each port in order to determine whether it's available.
    Also changed BaseBrainLinkControllable so that it now has two constructors.  The new constructor take a String which is a comma-delimited set of serial port names.  This allows the user to specify the serial ports to try via the constructor instead of only via the system property (e.g. by supplying the -D command line switch).  Any serial port names specified via the constructor are appended to any already specified via the system property.
    
    Finally, also added BrainLinkBlinkyLED test app.

--- trunk/software/java/code/core/src/edu/cmu/ri/createlab/brainlink/robots/BaseBrainLinkControllable.java
+++ trunk/software/java/code/core/src/edu/cmu/ri/createlab/brainlink/robots/BaseBrainLinkControllable.java
@@ -6,6 +6,7 @@ import edu.cmu.ri.createlab.brainlink.BrainLinkProxy;
 import edu.cmu.ri.createlab.device.connectivity.CreateLabDeviceConnectionEventListener;
 import edu.cmu.ri.createlab.device.connectivity.CreateLabDeviceConnectionState;
 import edu.cmu.ri.createlab.device.connectivity.CreateLabDeviceConnectivityManager;
+import edu.cmu.ri.createlab.serial.SerialPortEnumerator;
 import org.apache.log4j.Logger;
 
 /**
@@ -21,71 +22,106 @@ public abstract class BaseBrainLinkControllable implements BrainLinkControllable
 
    private BrainLink brainLink = null;
 
-   public BaseBrainLinkControllable()
+   /**
+    * Creates the <code>BaseBrainLinkControllable</code> by attempting to connect to a BrainLink on the given serial
+    * port(s).  Note that if one ore more serial ports is already specified as a system property (e.g. by using the -D
+    * command line switch), then the serial port(s) specified in the argument to this constructor are appended to the
+    * port names specified in the system property, and the system property value is updated.
+    */
+   public BaseBrainLinkControllable(final String userDefinedSerialPortNames)
+   {
+   if (userDefinedSerialPortNames != null && userDefinedSerialPortNames.trim().length() > 0)
       {
-      System.out.println("Connecting to BrainLink...this may take a few seconds...");
+      LOG.debug("BaseBrainLinkControllable.BaseBrainLinkControllable(): processing user-defined serial port names...");
 
-      connectivityManager.addConnectionEventListener(
-            new CreateLabDeviceConnectionEventListener()
-            {
-            public void handleConnectionStateChange(final CreateLabDeviceConnectionState oldState, final CreateLabDeviceConnectionState newState, final String portName)
-               {
-               if (CreateLabDeviceConnectionState.CONNECTED.equals(newState))
-                  {
-                  LOG.debug("BaseBrainLinkControllable.handleConnectionStateChange(): Connected");
+      // initialize the set of names to those specified in the argument to this constructor
+      final StringBuilder serialPortNames = new StringBuilder();
 
-                  // connection complete, so release the lock
-                  connectionCompleteSemaphore.release();
-                  brainLink = (BrainLinkProxy)connectivityManager.getCreateLabDeviceProxy();
-                  }
-               else if (CreateLabDeviceConnectionState.DISCONNECTED.equals(newState))
-                  {
-                  LOG.debug("BaseBrainLinkControllable.handleConnectionStateChange(): Disconnected");
-                  brainLink = null;
-                  }
-               else if (CreateLabDeviceConnectionState.SCANNING.equals(newState))
-                  {
-                  LOG.debug("BaseBrainLinkControllable.handleConnectionStateChange(): Scanning...");
-                  }
-               else
-                  {
-                  LOG.error("BaseBrainLinkControllable.handleConnectionStateChange(): Unexpected CreateLabDeviceConnectionState [" + newState + "]");
-                  brainLink = null;
-                  }
-               }
-            });
-
-      LOG.trace("BrainLink.BrainLink(): 1) aquiring connection lock");
-
-      // acquire the lock, which will be released once the connection is complete
-      connectionCompleteSemaphore.acquireUninterruptibly();
-
-      LOG.trace("BrainLink.BrainLink(): 2) connecting");
-
-      // try to connect
-      connectivityManager.scanAndConnect();
-
-      LOG.trace("BrainLink.BrainLink(): 3) waiting for connection to complete");
-
-      // try to acquire the lock again, which will block until the connection is complete
-      connectionCompleteSemaphore.acquireUninterruptibly();
-
-      LOG.trace("BrainLink.BrainLink(): 4) releasing lock");
-
-      // we know the connection has completed (i.e. either connected or the connection failed) at this point, so just release the lock
-      connectionCompleteSemaphore.release();
-
-      LOG.trace("BrainLink.BrainLink(): 5) make sure we're actually connected");
-
-      // if we're not connected, then throw an exception
-      if (!CreateLabDeviceConnectionState.CONNECTED.equals(connectivityManager.getConnectionState()))
+      // Now see if there are also serial ports already specified in the system property (e.g. via the -D command line switch).
+      // If so, then those take precedence the ones specified in the constructor argument will be appended
+      final String serialPortNamesAlreadyInSystemProperty = System.getProperty(SerialPortEnumerator.SERIAL_PORTS_SYSTEM_PROPERTY_KEY, null);
+      if (serialPortNamesAlreadyInSystemProperty != null && serialPortNamesAlreadyInSystemProperty.trim().length() > 0)
          {
-         LOG.error("BrainLink.BrainLink(): Failed to connect to the BrainLink!  Aborting.");
-         System.exit(1);
+         serialPortNames.append(serialPortNamesAlreadyInSystemProperty).append(',').append(userDefinedSerialPortNames);
          }
 
-      LOG.trace("BrainLink.BrainLink(): 6) All done!");
+      // now set the system property
+      System.setProperty(SerialPortEnumerator.SERIAL_PORTS_SYSTEM_PROPERTY_KEY, serialPortNames.toString());
+      LOG.debug("BaseBrainLinkControllable.BaseBrainLinkControllable(): System property [" + SerialPortEnumerator.SERIAL_PORTS_SYSTEM_PROPERTY_KEY + "] set to [" + serialPortNames + "]");
       }
+
+   System.out.println("Connecting to BrainLink...this may take a few seconds...");
+
+   connectivityManager.addConnectionEventListener(
+         new CreateLabDeviceConnectionEventListener()
+         {
+         public void handleConnectionStateChange(final CreateLabDeviceConnectionState oldState, final CreateLabDeviceConnectionState newState, final String portName)
+            {
+            if (CreateLabDeviceConnectionState.CONNECTED.equals(newState))
+               {
+               LOG.debug("BaseBrainLinkControllable.handleConnectionStateChange(): Connected");
+
+               // connection complete, so release the lock
+               connectionCompleteSemaphore.release();
+               brainLink = (BrainLinkProxy)connectivityManager.getCreateLabDeviceProxy();
+               }
+            else if (CreateLabDeviceConnectionState.DISCONNECTED.equals(newState))
+               {
+               LOG.debug("BaseBrainLinkControllable.handleConnectionStateChange(): Disconnected");
+               brainLink = null;
+               }
+            else if (CreateLabDeviceConnectionState.SCANNING.equals(newState))
+               {
+               LOG.debug("BaseBrainLinkControllable.handleConnectionStateChange(): Scanning...");
+               }
+            else
+               {
+               LOG.error("BaseBrainLinkControllable.handleConnectionStateChange(): Unexpected CreateLabDeviceConnectionState [" + newState + "]");
+               brainLink = null;
+               }
+            }
+         });
+
+   LOG.trace("BrainLink.BrainLink(): 1) aquiring connection lock");
+
+   // acquire the lock, which will be released once the connection is complete
+   connectionCompleteSemaphore.acquireUninterruptibly();
+
+   LOG.trace("BrainLink.BrainLink(): 2) connecting");
+
+   // try to connect
+   connectivityManager.scanAndConnect();
+
+   LOG.trace("BrainLink.BrainLink(): 3) waiting for connection to complete");
+
+   // try to acquire the lock again, which will block until the connection is complete
+   connectionCompleteSemaphore.acquireUninterruptibly();
+
+   LOG.trace("BrainLink.BrainLink(): 4) releasing lock");
+
+   // we know the connection has completed (i.e. either connected or the connection failed) at this point, so just release the lock
+   connectionCompleteSemaphore.release();
+
+   LOG.trace("BrainLink.BrainLink(): 5) make sure we're actually connected");
+
+   // if we're not connected, then throw an exception
+   if (!CreateLabDeviceConnectionState.CONNECTED.equals(connectivityManager.getConnectionState()))
+      {
+      LOG.error("BrainLink.BrainLink(): Failed to connect to the BrainLink!  Aborting.");
+      System.exit(1);
+      }
+
+   LOG.trace("BrainLink.BrainLink(): 6) All done!");
+   }
+
+   /**
+    * Creates the <code>BaseBrainLinkControllable</code> by attempting to connect to all available serial ports and
+    * connecting to the first BrainLink it finds.
+    */
+   public BaseBrainLinkControllable()
+   {
+   this(null);
+   }
 
    public final BrainLink getBrainLink()
       {
