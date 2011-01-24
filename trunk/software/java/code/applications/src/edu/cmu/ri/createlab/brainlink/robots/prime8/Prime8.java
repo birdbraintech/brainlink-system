8a5403ed51dcf779bc6c5eb73575bb3b4a1881e0
tlauwers@gmail.com <tlauwers@gmail.com@85f2b622-2cfc-f667-9a34-de0f26db76e9>
2011-01-24 20:15:17 +0000

    Put javadoc style comments in

--- trunk/software/java/code/applications/src/edu/cmu/ri/createlab/brainlink/robots/prime8/Prime8.java
+++ trunk/software/java/code/applications/src/edu/cmu/ri/createlab/brainlink/robots/prime8/Prime8.java
@@ -17,7 +17,10 @@ public final class Prime8 extends BaseBrainLinkControllable
       {
       this(null);
       }
-
+        /**  Instantiates a Prime8 class
+        *
+        * @param serialPortNames The serial port identified of the Brainlink connected to this computer
+        */
    public Prime8(final String serialPortNames)
       {
       super(serialPortNames);
@@ -29,37 +32,58 @@ public final class Prime8 extends BaseBrainLinkControllable
          brainLink.initializeIR(Prime8Constants.getInitializationCommand());
          }
       }
-
+        /** Prime8 will turn left until a second movement command or stop is sent
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean turnLeft()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.TURN_LEFT_COMMAND_STRATEGY);
       }
-
+         /** Prime8 will turn right until a second movement command or stop is sent
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean turnRight()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.TURN_RIGHT_COMMAND_STRATEGY);
       }
-
+        /** Prime8 will walk forward until a second movement command or stop is sent
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean walkForward()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.WALK_FORWARD_COMMAND_STRATEGY);
       }
-
+       /** Prime8 will walk backward until a second movement command or stop is sent
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean walkBackward()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.WALK_BACKWARD_COMMAND_STRATEGY);
       }
-
+         /** Prime8 will stop its motion
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean stop()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.STOP_COMMAND_STRATEGY);
       }
-
+       /** Prime8 will extend its small legs, causing it to stand up
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean standUp()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.STAND_UP_COMMAND_STRATEGY);
       }
-
+       /** Prime8 will retract its small legs, causing it to sit down
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean sitDown()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.SIT_DOWN_COMMAND_STRATEGY);
@@ -69,12 +93,18 @@ public final class Prime8 extends BaseBrainLinkControllable
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.GROWL_COMMAND_STRATEGY);
       }
-
+        /** Prime8 will guard an area and shoot at anyone it sees
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean guard()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.GUARD_COMMAND_STRATEGY);
       }
-
+         /** Prime8 will shoot its missiles
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean shoot()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.SHOOT_COMMAND_STRATEGY);
@@ -84,12 +114,18 @@ public final class Prime8 extends BaseBrainLinkControllable
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.GOOD_GORILLA_COMMAND_STRATEGY);
       }
-
+        /** Preprogrammed Demo
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean demo()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.DEMO_COMMAND_STRATEGY);
       }
-
+       /** Prime8 will roam the area and avoid obstacles
+        *
+        * @return true if the call was made successfully, false otherwise
+        */
    public boolean autonomousMode()
       {
       return executeCommandStrategy(Prime8Constants.CommandStrategies.AUTONOMOUS_COMMAND_STRATEGY);
--- trunk/software/java/code/applications/src/edu/cmu/ri/createlab/brainlink/robots/roomba/Roomba.java
+++ trunk/software/java/code/applications/src/edu/cmu/ri/createlab/brainlink/robots/roomba/Roomba.java
@@ -18,6 +18,10 @@ public final class Roomba extends BaseBrainLinkControllable
       this(null);
       }
 
+       /** Instantiates a Roomba class
+        *
+        * @param serialPortNames the serial port identifier on which the Brainlink can be found
+        */
    public Roomba(final String serialPortNames)
       {
       super(serialPortNames);
@@ -30,46 +34,75 @@ public final class Roomba extends BaseBrainLinkControllable
          }
       }
 
+       /** Turns roomba left until another movement method is called
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean turnLeft()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.TURN_LEFT_COMMAND_STRATEGY);
       }
-
+        /** Turns roomba right until another movement method is called
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean turnRight()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.TURN_RIGHT_COMMAND_STRATEGY);
       }
-
+       /** Drives roomba forward until another movement method is called
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean driveForward()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.DRIVE_FORWARD_COMMAND_STRATEGY);
       }
-
+      /** Sends the roomba clean command
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean clean()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.CLEAN_COMMAND_STRATEGY);
       }
-
+       /** Tells roomba to clean until it runs out of batteries
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean max()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.MAX_COMMAND_STRATEGY);
       }
-
+         /** Tells Roomba to do a spot clean
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean spot()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.SPOT_COMMAND_STRATEGY);
       }
-
+       /** Turns off Roomba.  You may need to manually turn roomba back on after sending this command.
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean powerOff()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.POWER_OFF_COMMAND_STRATEGY);
       }
 
+       /** Causes roomba to look for its charging dock
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean dock()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.DOCK_COMMAND_STRATEGY);
       }
-
+       /** Causes Roomba to stop moving
+        *
+        * @return  true if the call was made successfully, false otherwise
+        */
    public boolean stop()
       {
       return executeCommandStrategy(RoombaConstants.CommandStrategies.STOP_COMMAND_STRATEGY);
