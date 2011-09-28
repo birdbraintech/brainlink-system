b42cbcb6e207deb48d02fb2e3a12ed7b288bc836
tlauwers@gmail.com <tlauwers@gmail.com@85f2b622-2cfc-f667-9a34-de0f26db76e9>
2011-09-28 15:08:28 +0000

    Fixed multiple bugs in RawIR command strategy.

--- software/java/code/core/src/edu/cmu/ri/createlab/brainlink/commands/SendRawIRCommandStrategy.java
+++ software/java/code/core/src/edu/cmu/ri/createlab/brainlink/commands/SendRawIRCommandStrategy.java
@@ -18,10 +18,12 @@ public class SendRawIRCommandStrategy extends CreateLabSerialDeviceNoReturnValue
       {
       this.command = new byte[signal.length * 2 + 4];
       this.command[0] = COMMAND_PREFIX;
-      this.command[1] = getLowByteFromInt(signal.length);
+      this.command[1] = getLowByteFromInt((signal.length*2+1));
       int j = 2;
       for (int i = 0; i < signal.length; i++)
          {
+    	  // The signal is in microseconds, but the Brainlink expects it in increments of 2 us, so we divide by 2
+    	  signal[i] /= 2;
          this.command[j] = getHighByteFromInt(signal[i]);
          j++;
          this.command[j] = getLowByteFromInt(signal[i]);
